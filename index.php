56fc28c24e9ebe39c5eb33aba5436f7003ccb601
Aquila Afuadajo <aquilaafuadajo@gmail.com>
2020-07-17 11:36:04 +0100

    completed project, replaced wow.js with animate on scroll, and replaced all blurry images

--- index.php
+++ index.php
@@ -58,19 +58,19 @@
   <div class="container-fluid">
     <section class="hero">
       <div class="container pd-20">
-        <img src="./assets/images/A Logo 1 (1).png" alt="algorithmist logo" class="logo animate__animated animate__fadeInLeft">
+        <img src="./assets/images/A Logo 1 (1).png" alt="algorithmist logo" class="logo" data-aos="fade-right">
         <div class="row">
-          <div class="col-sm top-form-section animate__animated animate__fadeInUp animate__slow">
-            <h1 class="lg-text-1" >Code smart with <span style="font-weight: 700;">Algorithimist</span></h1>
-            <h2 class="lg-text-2">Coming Soon!</h2>
-            <img src="./assets/images/Frame 1 1.png" alt="" class="w-100 sm-screen-img">
-            <form action="index.php" method="POST">
+          <div class="col-sm top-form-section">
+            <h1 class="lg-text-1" data-aos="fade-up">Code smart with <span style="font-weight: 700;">Algorithimist</span></h1>
+            <h2 class="lg-text-2" data-aos="fade-up">Coming Soon!</h2>
+            <img src="./assets/images/Frame 1 1.png" alt="" class="w-100 sm-screen-img" data-aos="fade-up">
+            <form action="index.php" method="POST" data-aos="fade-up">
               <p class="sm-text">Want to get notified when our app is out?</p>
               <input type="text" name="email" class="custom-input" placeholder="Enter your email" value="<?php echo htmlspecialchars($email); ?>">
               <div class="red-text">
                 <?php echo $errors['email']; ?>
               </div>
-              <input type="submit" name="submit" value="NOTIFY ME" class="submit-btn wow animate__animated animate__bounce">
+              <input type="submit" name="submit" value="NOTIFY ME" class="submit-btn wow">
             </form>
             <!-- <form action="" class="form">
               <p class="sm-text">Want to get notified when our app is out?</p>
@@ -79,7 +79,7 @@
             </form> -->
           </div>
           <div class="col-sm">
-            <img src="./assets/images/Frame 1 1.png" alt="" class="w-100 lg-screen-img animate__animated animate__lightSpeedInRight">
+            <img src="./assets/images/Frame 1 1.png" alt="" class="w-100 lg-screen-img" data-aos="fade-left">
           </div>
         </div>
       </div>
@@ -87,10 +87,10 @@
     
     <section class="content bg-white">
       <div class="container">
-        <h3 class="custom-h3 mg-btm-30">Get quick code solutions for your challenges</h3>
+        <h3 class="custom-h3 mg-btm-30" data-aos="zoom-in-up">Get quick code solutions for your challenges</h3>
         <div class="pd-top-bottom row">
           <div class="col-md">
-            <p class="m-text pd-top-bottom">Ever wanted a quick fix in creating your codes?
+            <p class="m-text pd-top-bottom" data-aos="fade-right">Ever wanted a quick fix in creating your codes?
               Algorithimist provides you with a faster approach  
               to create a thousand lines of code to solve any problem.
               It is an Artificial Intelligence application that solves real life 
@@ -99,7 +99,7 @@
             </p>
           </div>
           <div class="col-md">
-            <div class="code-editor">
+            <div class="code-editor" data-aos="fade-up-left">
               <div id="typed-strings" style="font-family: 'Courier New', Courier, monospace !important;">
     
                 <code><span class="purple"">function</span> <span class="blue">algorithmist</span><span class="grey">(keywords)</span> { <br />
@@ -127,64 +127,64 @@
     </section>
 
     <section class="content wte bg-white">
-      <h3 class="custom-h3 mg-btm-30">What to expect</h3>
+      <h3 class="custom-h3 mg-btm-30" data-aos="zoom-in-up">What to expect</h3>
       <div class="container lg-screens">
         <div class="row">
-          <div class="col-sm wow animate_animated animate__bounceInUp">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 9 (3).png" alt="" class="pd-top">
+          <div class="col-sm">
+            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 9 (3).png" alt="" class="pd-top" data-aos="zoom-in-up">
           </div>
-          <div class="col-sm wow animate_animated animate__bounceInUp animate__delay-1s">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 5 (3).png" alt="" class="">
+          <div class="col-sm">
+            <img src="./assets/images/Screen 2 10.png" alt="" class="" data-aos="flip-left">
           </div>
-          <div class="col-sm wow animate_animated animate__bounceInUp animate__delay-2s">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 4 (2).png" alt="" class="pd-top">
+          <div class="col-sm">
+            <img src="./assets/images/Screen 2 1.png" alt="" class="pd-top" data-aos="zoom-in-up">
           </div>
         </div>
         <div class="row">
           <div class="col-sm">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 6 (1).png" alt="" class="pd-top">
+            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 6 (1).png" alt="" class="pd-top" data-aos="fade-right">
           </div>
           <div class="col-sm">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 7 (1).png" alt="" class="">
+            <img src="./assets/images/Screen 2 11.png" alt="" class="" data-aos="zoom-in-up">
           </div>
           <div class="col-sm">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 8 (1).png" alt="" class="pd-top">
+            <img src="./assets/images/Screen 2 14.png" alt="" class="pd-top" data-aos="fade-left">
           </div>
         </div>
       </div>
       <div class="container tab-screens">
         <div class="row">
           <div class="col-sm">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 9 (4).png" alt="" class="pd-top">
+            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 9 (4).png" alt="" class="pd-top" data-aos="zoom-in-up">
           </div>
           <div class="col-sm">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 14.png" alt="" class="">
+            <img src="./assets/images/Screen 2 6.png" alt="" class="" data-aos="flip-left">
           </div>
           <div class="col-sm">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 10.png" alt="" class="pd-top">
+            <img src="./assets/images/Screen 2 2 (1).png" alt="" class="pd-top" data-aos="zoom-in-up">
           </div>
         </div>
         <div class="row">
           <div class="col-sm">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 12.png" alt="" class="pd-top">
+            <img src="./assets/images/Screen 2 4.png" alt="" class="pd-top" data-aos="fade-right">
           </div>
           <div class="col-sm">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 13.png" alt="" class="">
+            <img src="./assets/images/Screen 2 8.png" alt="" class="" data-aos="zoom-in-up">
           </div>
           <div class="col-sm">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 9 (5).png" alt="" class="pd-top">
+            <img src="./assets/images/Screen 2 15.png" alt="" class="pd-top" data-aos="fade-right">
           </div>
         </div>
       </div>
       <div class="container mobile-screens">
         <div class="row">
           <div class="col">
-            <img class="pd-btm-30" src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 9 (4).png" alt="">
-            <img class="pd-btm-30" src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 10.png" alt="">
-            <img class="pd-btm-30" src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 14.png" alt="">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 12.png" alt="" class="pd-btm-30">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 13.png" alt="" class="pd-btm-30">
-            <img src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 9 (5).png" alt="" class="pd-btm-30">
+            <img class="pd-btm-30" src="./assets/images/iPhone 8 - 1 (1)_iphone8spacegrey_portrait 9 (4).png" alt="" data-aos="flip-left">
+            <img class="pd-btm-30" src="./assets/images/Screen 2 2 (1).png" alt="" data-aos="fade-up-left">
+            <img class="pd-btm-30" src="./assets/images/Screen 2 6.png" alt="" data-aos="fade-up-right">
+            <img src="./assets/images/Screen 2 4.png" alt="" class="pd-btm-30" data-aos="zoom-in-up">
+            <img src="./assets/images/Screen 2 8.png" alt="" class="pd-btm-30" data-aos="fade-left">
+            <img src="./assets/images/Screen 2 15.png" alt="" class="pd-btm-30" data-aos="fade-right">
           </div>
         </div>
       </div>
@@ -192,28 +192,28 @@
 
     <section class="content bg-light-grey">
       <div class="container">
-        <h3 class="custom-h3 wow animate__animated animate__backInRight">Outputs result in over 50 languages</h3>
+        <h3 class="custom-h3" data-aos="zoom-in-up">Outputs result in over 50 languages</h3>
         <div class="container-fluid mg-20">
           <div class="grid-container--fill">
-            <div class="lang-card">
+            <div class="lang-card" data-aos="flip-left">
               <img src="./assets/images/new js.png" alt="" class="w-100">
             </div>
-            <div class="lang-card">
+            <div class="lang-card" data-aos="flip-left">
               <img src="./assets/images/new swift.png" alt="" class="w-100">
             </div>
-            <div class="lang-card">
+            <div class="lang-card" data-aos="flip-left">
               <img src="./assets/images/Python.png" alt="" class="w-100">
             </div>
-            <div class="lang-card">
+            <div class="lang-card" data-aos="flip-left">
               <img src="./assets/images/Java.png" alt="" class="w-100">
             </div>
-            <div class="lang-card">
+            <div class="lang-card" data-aos="flip-left">
               <img src="./assets/images/new ruby.png" alt="" class="w-100">
             </div>
-            <div class="lang-card">
+            <div class="lang-card" data-aos="flip-left">
               <img src="./assets/images/php.png" alt="" class="w-100">
             </div>
-            <div class="lang-card csharp">
+            <div class="lang-card csharp"  data-aos="flip-left">
               <img src="./assets/images/csharp.png" alt="" class="w-100">
             </div>
           </div>
@@ -223,9 +223,9 @@
 
     <section class="content bg-white">
       <div class="container">
-        <h3 class="custom-h3">Trusted by top global companies</h3>
+        <h3 class="custom-h3" data-aos="zoom-in-up">Trusted by top global companies</h3>
         <div class="container-fluid mg-20">
-          <div class="row companies">
+          <div class="row companies" data-aos="zoom-in-up">
             <div class="col-sm">
               <div class="row">
                 <div class="col center-align pd-img">
@@ -255,9 +255,9 @@
       <div class="container">
         <div class="row">
           <div class="col-md pd-0">
-            <h1 class="lg-text-3">Want to get notified?</h1>
+            <h1 class="lg-text-3" data-aos="fade-right">Want to get notified?</h1>
           </div>
-          <div class="col-md pd-0 mg-auto">
+          <div class="col-md pd-0 mg-auto" data-aos="fade-up">
             <div class="row btm-form">
               <form action="index.php" method="POST">
                 <input type="text" name="email" class="custom-input" placeholder="Enter your email" value="<?php echo htmlspecialchars($email); ?>">
